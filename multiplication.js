56d8bad988f7e72abaccb04d36b4db67dfa87bf9
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-19 22:18:08 +0600

    multiply 3 numbers using arrow function

---
+++ multiplication.js
@@ -0,0 +1,7 @@
+let multiplication = (number1, number2, number3) => number1 * number2 * number3;
+
+let result=multiplication(5,3,2);
+console.log(result);
+
+let result2=multiplication(12,2,2);
+console.log(result2);
