707cf9867f4c7d6e14c70dca9f78e3e08c786583
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-23 05:25:02 +0600

    find and filter function for finding numbers divisible by 10

--- findDivisibleByTen.js
+++ findDivisibleByTen.js
@@ -1,3 +1,8 @@
+//find numbers divisible by 10 using filter
+// difference is in filter it returns array on the other hand find returns a single number
 let numbers = [33, 50, 79, 78, 90, 101, 30];
-let divisibleByTen = numbers.filter((x) => x % 10 == 0);
+let divisibleByTenNumbers = numbers.filter((x) => x % 10 == 0);
+console.log(divisibleByTenNumbers);
+//find numbers divisible by 10 using find
+let divisibleByTen = numbers.find((x) => x % 10 == 0);
 console.log(divisibleByTen);
